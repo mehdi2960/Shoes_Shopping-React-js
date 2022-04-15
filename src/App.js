19712dca8b0e41cd02c9a295f88dbee3f05ed5fd
mehdi <mehdiprogrammer30@gmail.com>
2022-04-15 22:23:19 +0430

    set icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {CssBaseline} from "@material-ui/core";
-import second from "first";
 import RTL from "./components/RTL";
 import ThemeProvider from './components/ThemeProvider'
 import MasterLayout from './screens/MasterLayout'
