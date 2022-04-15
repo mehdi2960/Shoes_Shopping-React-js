443a66ead17b40d6416aef03c34621ccd3e55e69
mehdi <mehdiprogrammer30@gmail.com>
2022-04-15 22:07:43 +0430

    project start

---
+++ src/components/ThemeProvider/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {ThemeProvider} from '@material-ui/core'
+import Theme from '../../theme/theme'
+
+
+
+export default function index({children}) {
+  return (
+    <ThemeProvider theme={Theme}>
+        {children}
+    </ThemeProvider>
+  )
+}
