029da78e8d960b2961d645bbb9f84a06c562040f
mehdi <mehdiprogrammer30@gmail.com>
2022-04-16 00:58:48 +0430

    menus - footer

--- src/screens/MasterLayout.js
+++ src/screens/MasterLayout.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Hidden } from '@material-ui/core'
+import useStyles from './MasterLayout.styles'
+import DesktopMenu from '../components/Menus/desktop'
+import MobileMenu from '../components/Menus/mobile'
+import Footer from '../components/Menus/Footer'
+
 
 export default function MasterLayout() {
+  const classes=useStyles()
   return (
-    <div>MasterLayout</div>
+    <BrowserRouter>
+       <div className={classes.root}>
+          <Hidden xsDown>
+            <DesktopMenu/>
+          </Hidden>
+           <Hidden smUp>
+            <MobileMenu/>
+          </Hidden>
+       </div>
+       <Footer/>
+    </BrowserRouter>
   )
 }
---
+++ src/theme/pallet.js
@@ -0,0 +1,17 @@
+export default{
+    primary:{
+        main:'#324958',
+        contrastText:'#fff'
+    },
+    secondary:{
+        main:"#263943",
+        contrastText:'#87e4b0'
+    },
+    text:{
+        primary:"#7d8591",
+        secondary:"#25d9da",
+        disabled:"#b18080",
+        title:"#3c4859",
+        caption:"#dbe0e9"
+    }
+}
